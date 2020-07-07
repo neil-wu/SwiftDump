630499b9ccefec16561866236abf653fc9bb2a08
neilwu <wuyupeng2007@gmail.com>
2020-07-07 12:45:41 +0800

    init

--- SwiftDump/SwiftDump/MachO/LoadCommand.swift
+++ SwiftDump/SwiftDump/MachO/LoadCommand.swift
@@ -1,3 +1,18 @@
+/*
+ Copyright Geoffrey Foster
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 //
 //  LoadCommand.swift
 //  Machismo
@@ -5,6 +20,7 @@
 //  Created by Geoffrey Foster on 2018-05-06.
 //  Copyright © 2018 g-Off.net. All rights reserved.
 //
+// neilwu modify this file for SwiftDump
 
 import Foundation
 
